6668ad4c375e0f6d4f9e9308024cb668c48c7f74
Sagar <sagargulati471@gmail.com>
2026-07-25 02:41:30 +0530

    Adds code for DB fetch candles functions | ingest_into_db function |  the Python model for indicator

--- Project-Root/consumers/indicator/handler.py
+++ Project-Root/consumers/indicator/handler.py
@@ -4,7 +4,8 @@ import asyncpg
 import asyncio
 
 from config.config import Config
-from ..candle_builder.models import Candle, NormalizedTrade
+from .models import Indicator
+from ..candle_builder.models import Candle
 
 config = Config()
 logger = logging.getLogger(__name__)
@@ -15,7 +16,7 @@ def make_handler(pool, producer):
     Factory function that creates the message handler with bound resources.
 
     This pattern allows us to inject shared dependencies (DB pool, Kafka producer)
-    into the handler without making them global variables or passing them around
+    into the handler without making them global variables or passing them we
     explicitly in the consumer loop. The returned handle_message function has
     access to these resources via closure scope.
     """
@@ -32,13 +33,107 @@ def make_handler(pool, producer):
             logger.exception(f"Failed to parse message: {e} raw={raw}")
             return
 
-        
 
-        
-        
-
-
-    return handle_message
 
 
     
+    return handle_message
+
+
+# Async function to fetch a candle from the database
+async def fetch_candle_from_db(pool, symbol, resolution, timestamp, max_candles_to_fetch = 200):
+    """
+    Fetch a candle from the database based on the provided symbol, resolution, and timestamp.
+    Returns a Candle object if found, otherwise returns None.
+    """
+    async with pool.acquire() as connection:
+
+        try:
+
+            # *********************** Note on SQL Injection Prevention ***********************
+            # Initially I thought of constructing the query string directly, but using parameterized queries
+            # is safer and avoids SQL injection risks.
+            # Eg: The SQL injection can happen like -
+            # symbol = "'; DROP TABLE candles; --"
+
+            # Using parameterized queries (with $1, $2, etc.) is a best practice to prevent SQL injection attacks.
+            # It ensures that user input is treated as data, not executable code.
+            # This is especially important in applications that interact with databases, as it helps maintain the integrity and security
+
+
+            # DB_QUERY = f"""SELECT * FROM candles
+            # WHERE symbol = '{symbol}' AND resolution = '{resolution}' AND timestamp < '{timestamp}'
+            # ORDER BY timestamp DESC
+            # LIMIT {max_candles_to_fetch}"""
+            # logger.debug(f"Executing DB query: {DB_QUERY}")
+
+            DB_QUERY = """
+                SELECT *
+                FROM candles
+                WHERE symbol = $1
+                AND resolution = $2
+                AND open_time < $3
+                ORDER BY open_time DESC
+                LIMIT $4
+                """
+
+            result = await connection.fetch(
+                DB_QUERY,
+                symbol,
+                resolution,
+                timestamp,
+                max_candles_to_fetch
+            )
+            # if result:
+            #     return Candle(**result)
+            return None
+        except Exception as e:
+            logger.exception(f"Database query failed:{e}, Query={DB_QUERY}, parameters=({symbol}, {resolution}, {timestamp}, {max_candles_to_fetch})")
+            return None
+
+
+async def ingest_into_db(pool: asyncpg.Pool, indicator: Indicator) -> None:
+        """
+        Inserts the Indicator into the Indicator hypertable.
+        """
+
+        INSERT_QUERY="""      
+        INSERT INTO indicator (symbol, resolution, open_time, vwap_session, sma_9, sma_21, sma_50, sma_200, ema_9, ema_21, ema_50, ema_200, rsi_14, macd_line, macd_signal, macd_histogram, bb_upper, bb_middle, bb_lower, bb_bandwidth)
+        VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17,$18,$19,$20)
+        ON CONFLICT (symbol,resolution,open_time) DO NOTHING
+        """
+        try:
+            async with pool.acquire() as conn:
+                  await conn.execute(
+                      INSERT_QUERY,
+                      indicator.symbol,
+                      indicator.resolution,
+                      indicator.open_time,
+                      indicator.vwap_session,
+                      indicator.sma_9,
+                      indicator.sma_21,
+                      indicator.sma_50,
+                      indicator.sma_200,
+                      indicator.ema_9,
+                      indicator.ema_21,
+                      indicator.ema_50,
+                      indicator.ema_200,
+                      indicator.rsi_14,
+                      indicator.macd_line,
+                      indicator.macd_signal,
+                      indicator.macd_histogram,
+                      indicator.bb_upper,
+                      indicator.bb_middle,
+                      indicator.bb_lower,
+                      indicator.bb_bandwidth,
+                  )
+            logger.debug(
+                f"Indicator persisted: symbol={indicator.symbol} "
+                f"open_time={indicator.open_time.isoformat()}"
+          )
+        except Exception as w:
+            logger.exception(
+                f"Failed to persist indicator to DB: "
+                f"symbol={indicator.symbol} open_time={indicator.open_time.isoformat()}"
+            )
+            raise
--- Project-Root/consumers/indicator/models.py
+++ Project-Root/consumers/indicator/models.py
@@ -1,4 +1,22 @@
-from pydantic import BaseModel, Field, computed_field
+from pydantic import BaseModel, Field
+from typing import Optional
+import datetime
+
+
+
+
+# *************** Explanation of the Indicator model fields ***************
+# If there are not enough candles, then we can't compute that indicator. 
+# So, we will return None for that indicator.
+
+# Example: If we have only 5 candles, then we can't compute SMA_9, SMA_21, SMA_50, SMA_200, EMA_9, EMA_21, EMA_50, EMA_200, RSI_14, MACD_Line, MACD_Signal, MACD_Histogram, BB_Upper, BB_Middle, BB_Lower, BB_Bandwidth.
+# In this case all the indicators will be None, except for VWAP_Session which can be computed with any number of candles.
+
+# Syntax - sma_9: float | None = Field(default=None, alias="sma_9") 
+# Means - the field sma_9 can either be a float or None.
+# If no value is provided, it defaults to None. The alias "sma_9" is used for serialization/deserialization.
+# alias="sma_9" is basically if JSon has a key "sma_9", it will be mapped to the field sma_9 in the Indicator model (Python's object).
+# And we will access it as indicator.sma_9 only in the code.
 
 
 class Indicator(BaseModel):
@@ -6,36 +24,36 @@ class Indicator(BaseModel):
     # Identifier fields
     symbol: str
     resolution: str
-    timestamp: int
+    open_time:  datetime.datetime
 
     # Indicator fields
     vwap_session: float = Field(alias='vwap_session')
 
     # Simple Moving Averages
-    sma_9: float = Field(alias='sma_9')
-    sma_21: float = Field(alias='sma_21')
-    sma_50: float = Field(alias='sma_50')
-    sma_200: float = Field(alias='sma_200')
+    sma_9: float | None = Field(default=None, alias="sma_9")
+    sma_21: float | None = Field(default=None, alias="sma_21")
+    sma_50: float | None = Field(default=None, alias="sma_50")
+    sma_200: float | None = Field(default=None, alias="sma_200")
 
     # Exponential Moving Averages
-    ema_9: float = Field(alias='ema_9')
-    ema_21: float = Field(alias='ema_21')
-    ema_50: float = Field(alias='ema_50')
-    ema_200: float = Field(alias='ema_200')
+    ema_9: float | None = Field(default=None, alias="ema_9")
+    ema_21: float | None = Field(default=None, alias="ema_21")
+    ema_50: float | None = Field(default=None, alias="ema_50")
+    ema_200: float | None = Field(default=None, alias="ema_200")
 
     # Relative Strength Index
-    rsi_14: float = Field(alias='rsi_14')
+    rsi_14: float | None = Field(default=None, alias="rsi_14")
 
     # Moving Average Convergence Divergence
-    macd_line: float = Field(alias='macd_line')
-    macd_signal: float = Field(alias='macd_signal')
-    macd_histogram: float = Field(alias='macd_histogram')
+    macd_line: float | None = Field(default=None, alias="macd_line")
+    macd_signal: float | None = Field(default=None, alias="macd_signal")
+    macd_histogram: float | None = Field(default=None, alias="macd_histogram")
 
     # Bollinger Bands
-    bb_upper: float = Field(alias='bb_upper')
-    bb_middle: float = Field(alias='bb_middle')
-    bb_lower: float = Field(alias='bb_lower')
-    bb_bandwidth: float = Field(alias='bb_bandwidth')
+    bb_upper: float | None = Field(default=None, alias="bb_upper")
+    bb_middle: float | None = Field(default=None, alias="bb_middle")
+    bb_lower: float | None = Field(default=None, alias="bb_lower")
+    bb_bandwidth: float | None = Field(default=None, alias="bb_bandwidth")
 
 
     
